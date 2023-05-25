9bcb1ee251b51dc38db1f9af2259fa9d8f89f6ce
Beltz Anhxton <beltz.18kyodai@gmail.com>
2023-05-25 16:16:33 -0400

    integrated PWA app into Nextjs App

---
+++ client/next.config.js
@@ -0,0 +1,12 @@
+const runtimeCaching = require("next-pwa/cache")
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+  disable:process.env.NODE_ENV === 'development'
+})
+const nextConfig = withPWA({})
+
+module.exports = nextConfig
